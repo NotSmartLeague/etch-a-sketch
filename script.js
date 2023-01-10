90bbad97a15e26f6ab64f7affd75caf41e61dfd9
Danilo <123456789+odin@users.noreply.github.com>
2023-01-10 20:31:42 +0100

    the grid can be created but not eliminated

--- script.js
+++ script.js
@@ -7,40 +7,45 @@ const modalColor = document.querySelector('.modal-color');
 const modalPixel = document.querySelector('.modal-pixel');
 
 function setInvisible(modal){
-    modal.classList.toggle("visible")
+    modal.classList.toggle("visible")       //declaring function to display the modal toggling the class visible on click
 }
-colorPicker.onclick = function(){
+colorPicker.onclick = function(){           //showing modal on click calling the function
     setInvisible(modalColor);
 }
-pixel.onclick = function (){
+pixel.onclick = function (){                //showing modal on click calling the funcion
     setInvisible(modalPixel);
 }
 
 
-/* rainbow.addEventListener("click", function(){})
-eraser.addEventListener("click", function(){})
-clear.addEventListener("click", function(){})
-pixel.addEventListener("click", function(){}) */
 
-const getPixel = document.getElementById('rangePixel');
-const targetNode = document.querySelector('.grid');
+
+const getPixel = document.getElementById('rangePixel');                 //selecting the element that gets the resolution
+const targetNode = document.querySelector('.grid');                     //selecting the node where the div is to be added
 
 getPixel.onclick = function(){
-    let resolution = getPixel.value * getPixel.value;
-    let width = 655 / getPixel.value;
-    let height = 655 / getPixel.value;
-    let newDiv;
-    if(newDiv == true){
-        for (let i = 0; i < resolution; i++){
-            console.log("working");
+    let resolution = getPixel.value * getPixel.value;                   //setting the resolution
+    let width = 655 / getPixel.value;                                   //calculating with of single pixel (655 is the dimention of the white board)
+    let height = 655 / getPixel.value;                                  //calculating heigh of single pixel 
+    let newDiv = document.querySelector('.grid-item');
+    let divToRemove = document.querySelectorAll('.grid-item')
+    //START OF NOT WORKING STUFF//
+    if(divToRemove != null){
+        for (let i = 0; i < resolution; i++){                           //it removes all the divs if they exist,                                    
+            divToRemove.remove();                
+            console.log("removed");
         }
     }
+    
     for(i = 0; i < resolution;i++){
         newDiv = document.createElement('div');
         newDiv.classList.add('grid-item');
-        newDiv.style.width = width + "px"; 
+        newDiv.style.width = width + "px";                              //creating every pixel, but only on the first click, next time i can try editing dimension with js 
         newDiv.style.height = height + "px"; 
+        console.log("div created")
         targetNode.appendChild(newDiv);
     }
+    console.log(newDiv)
     
 }
+//END OF NO WORKING STUFF//
+    
