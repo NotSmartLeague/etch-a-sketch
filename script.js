82b7b0f0984723c651634a43885d829d9d4edc88
Danilo <123456789+odin@users.noreply.github.com>
2023-01-10 22:25:54 +0100

    now i fixed the 'padding' problem

--- script.js
+++ script.js
@@ -7,39 +7,50 @@ const modalColor = document.querySelector('.modal-color');
 const modalPixel = document.querySelector('.modal-pixel');
 
 function setInvisible(modal){
-    modal.classList.toggle("visible")
+    modal.classList.toggle("visible")       //declaring function to display the modal toggling the class visible on click
 }
-colorPicker.onclick = function(){
+colorPicker.onclick = function(){           //showing modal on click calling the function
     setInvisible(modalColor);
 }
-pixel.onclick = function (){
+pixel.onclick = function (){                //showing modal on click calling the funcion
     setInvisible(modalPixel);
 }
 
 
-/* rainbow.addEventListener("click", function(){})
-eraser.addEventListener("click", function(){})
-clear.addEventListener("click", function(){})
-pixel.addEventListener("click", function(){}) */
 
-const getPixel = document.getElementById('rangePixel');
-const targetNode = document.querySelector('.grid');
+
+const getPixel = document.getElementById('rangePixel');                 //selecting the element that gets the resolution
+const targetNode = document.querySelector('.grid');                     //selecting the node where the div is to be added
 
 getPixel.onclick = function(){
-    let resolution = getPixel.value * getPixel.value;
-    let width = 655 / getPixel.value;
-    let height = 655 / getPixel.value;
-    let newDiv;
-    if(newDiv == true){
-        for (let i = 0; i < resolution; i++){
-            console.log("working");
+    let resolution = getPixel.value * getPixel.value;                   //setting the resolution
+    let width = 730 / getPixel.value;                                   //calculating with of single pixel (655 is the dimention of the white board)
+    let height = 730 / getPixel.value;                                  //calculating heigh of single pixel 
+    let newDiv = document.querySelector('.grid-item');
+    let divNodeList = document.querySelectorAll('.grid-item')
+    divNodeList.forEach(
+        function(){
+            let divToRemove = document.querySelector('.grid-item')
+            divToRemove.remove();
+            console.log("div removed")
         }
+        )
+        
+        for(i = 0; i < resolution;i++){
+            newDiv = document.createElement('div');
+            newDiv.classList.add('grid-item');
+            newDiv.style.width = width + "px";                              //this create every on the pixel, but only on the first click, have to work on it 
+            newDiv.style.height = height + "px"; 
+            newDiv.setAttribute("onmouseenter", "toColor(this)")
+            console.log("div created")
+            targetNode.appendChild(newDiv);
+        }
+        
     }
-    for(i = 0; i < resolution;i++){
-        newDiv = document.createElement('div');
-        newDiv.classList.add('grid-item');
-        newDiv.style.width = width + "px"; 
-        newDiv.style.height = height + "px"; 
-        targetNode.appendChild(newDiv);
+    let newDiv = document.querySelector('.grid-item');
+    function toColor(pixel){
+        pixel.style.backgroundColor = "black";
     }
-}
+    
+
+    
